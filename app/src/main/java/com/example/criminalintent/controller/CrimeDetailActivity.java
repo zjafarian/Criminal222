2f7490c5de5dbb982eddb32bd465d61d47feb779
ctrlslash <1ace28d67b>
2020-08-15 13:33:12 +0430

    fix logic for showing CrimeDetailFragment in CrimeDetailActivity.

--- app/src/main/java/com/example/criminalintent/controller/CrimeDetailActivity.java
+++ app/src/main/java/com/example/criminalintent/controller/CrimeDetailActivity.java
@@ -21,32 +21,13 @@ public class CrimeDetailActivity extends AppCompatActivity {
         //add CrimeDetailFragment to this activity (dynamic)
 
         //get fragment manager
-        final FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentManager fragmentManager = getSupportFragmentManager();
 
         //create an add fragment transaction for CrimeDetailFragment
-
-        final CrimeDetailFragment crimeDetailFragment = new CrimeDetailFragment();
-
-        Button button = findViewById(R.id.add_crime);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                getSupportFragmentManager()
-                        .beginTransaction()
-                        .add(R.id.fragment_container, crimeDetailFragment)
-                        .commit();
-            }
-        });
-
-        Button button1 = findViewById(R.id.remove_crime);
-        button1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                getSupportFragmentManager()
-                        .beginTransaction()
-                        .remove(crimeDetailFragment)
-                        .commit();
-            }
-        });
+        CrimeDetailFragment crimeDetailFragment = new CrimeDetailFragment();
+        getSupportFragmentManager()
+                .beginTransaction()
+                .add(R.id.fragment_container, crimeDetailFragment)
+                .commit();
     }
 }
--- app/src/main/java/com/example/criminalintent/controller/CrimeDetailFragment.java
+++ app/src/main/java/com/example/criminalintent/controller/CrimeDetailFragment.java
@@ -4,12 +4,20 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
 
 import androidx.fragment.app.Fragment;
 
 import com.example.criminalintent.R;
 
 public class CrimeDetailFragment extends Fragment {
+
+    private EditText mEditTextTitle;
+    private Button mButtonDate;
+    private CheckBox mCheckBoxSolved;
 
     public CrimeDetailFragment() {
         // Required empty public constructor
@@ -29,6 +37,35 @@ public class CrimeDetailFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_crime_detail, container, false);
+
+        findViews(view);
+
+        mButtonDate.setEnabled(false);
+
+        setListeners();
+
         return view;
+    }
+
+    private void findViews(View view) {
+        mEditTextTitle = view.findViewById(R.id.crime_title);
+        mButtonDate = view.findViewById(R.id.crime_date);
+        mCheckBoxSolved = view.findViewById(R.id.crime_solved);
+    }
+
+    private void setListeners() {
+        mCheckBoxSolved.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+            }
+        });
+
+        mButtonDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
