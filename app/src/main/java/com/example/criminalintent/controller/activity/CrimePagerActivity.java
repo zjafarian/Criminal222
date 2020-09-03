2906f0ec022bfea7dd7b021e1dc7b417b3cf2d4b
zjafarian <zjafarian88@gmail.com>
2020-09-03 20:23:29 +0430

    Add animation to view pager. (zoom animation)

--- app/src/main/java/com/example/criminalintent/controller/activity/CrimePagerActivity.java
+++ app/src/main/java/com/example/criminalintent/controller/activity/CrimePagerActivity.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,6 +12,7 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
 import androidx.fragment.app.FragmentPagerAdapter;
 import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 import androidx.viewpager2.adapter.FragmentStateAdapter;
 import androidx.viewpager2.widget.ViewPager2;
 
@@ -46,8 +48,8 @@ public class CrimePagerActivity extends AppCompatActivity {
 
         mRepository = CrimeRepository.getInstance();
         mCrimeId = (UUID) getIntent().getSerializableExtra(EXTRA_CRIME_ID);
-
         findViews();
+        mViewPagerCrimes.setPageTransformer(new ZoomOutPageTransFormer());
         initViews();
     }
 
@@ -101,6 +103,38 @@ public class CrimePagerActivity extends AppCompatActivity {
         @Override
         public int getItemCount() {
             return mCrimes.size();
+        }
+    }
+
+    private class ZoomOutPageTransFormer implements ViewPager2.PageTransformer {
+        private static final float MIN_SCALE = 0.85f;
+        private static final float MIN_ALPHA = 0.5f;
+
+
+        @Override
+        public void transformPage(@NonNull View page, float position) {
+            int pageWidth= page.getWidth();
+            int pageHeight = page.getHeight();
+            if (position<-1){
+                page.setAlpha(0F);
+            } else if (position <=1){
+                float scaleFactor = Math.max(MIN_SCALE,1 - Math.abs(position));
+                float vertMargin = pageHeight * (1 - scaleFactor) / 2;
+                float horzMargin = pageWidth * (1 - scaleFactor) / 2;
+                if (position < 0){
+                    page.setTranslationX(horzMargin - vertMargin / 2);
+                } else {
+                    page.setTranslationX(-horzMargin + vertMargin / 2);
+                }
+                page.setScaleX(scaleFactor);
+                page.setScaleY(scaleFactor);
+                page.setAlpha(MIN_ALPHA + (scaleFactor - MIN_SCALE) / (1 - MIN_SCALE) *
+                        (1 - MIN_ALPHA));
+
+            } else {
+                page.setAlpha(0F);
+            }
+
         }
     }
 }
