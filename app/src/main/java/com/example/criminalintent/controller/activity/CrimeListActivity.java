888c89dd0261d459e5cd2b766c7294953bf39dc2
zjafarian <zjafarian88@gmail.com>
2020-09-25 13:44:33 +0330

    Use notifydataset only for one item in CrimeListFragment

--- app/src/main/java/com/example/criminalintent/controller/activity/CrimeListActivity.java
+++ app/src/main/java/com/example/criminalintent/controller/activity/CrimeListActivity.java
@@ -9,13 +9,18 @@ import com.example.criminalintent.controller.fragment.CrimeListFragment;
 
 public class CrimeListActivity extends SingleFragmentActivity {
 
-    public static Intent newIntent(Context context) {
+    public static final String EXTRA_CURRENT_INDEX = "com.example.criminalintent.CurrentIndex";
+
+    public static Intent newIntent(Context context, int index) {
         Intent intent = new Intent(context, CrimeListActivity.class);
+        intent.putExtra(EXTRA_CURRENT_INDEX,index);
         return intent;
     }
 
     @Override
     public Fragment createFragment() {
-        return CrimeListFragment.newInstance();
+        int index = getIntent().getIntExtra(EXTRA_CURRENT_INDEX,0);
+        CrimeListFragment crimeListFragment = CrimeListFragment.newInstance(index);
+        return crimeListFragment;
     }
 }
